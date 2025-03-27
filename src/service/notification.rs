d1cbf23f80f87c4da52b70970718f2ec9f4ee664
maskrio <yosemaranata@gmail.com>
2025-03-27 22:05:45 +0700

    Implement unsubscribe function in Notification service.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -5,7 +5,7 @@ use rocket::log;
 use rocket::serde::json::to_string;
 use rocket::tokio;
 
-use bambangshop_receiver::{ APP_CONFIG, REQUEST_CLIENT, Result, compose_error_response };
+use bambangshop_receiver::{ APP_CONFIG, REQWEST_CLIENT, Result, compose_error_response };
 use crate::model::notification::Notification;
 use crate::model::subscriber::SubscriberRequest;
 use crate::repository::notification::NotificationRepository;
@@ -39,18 +39,63 @@ impl NotificationService {
             APP_CONFIG.get_publisher_root_url(),
             product_type_str
         );
-        let request = REQUEST_CLIENT.post(request_url.clone())
+        let request = REQWEST_CLIENT.post(request_url.clone())
             .header("Content-Type", "application/json")
             .header("Accept", "application/json")
             .body(to_string(&payload).unwrap())
             .send().await;
-        log::warn!("Sent subscribe request to: {}", request_url);
+        log::warn_!("Sent subscribe request to: {}", request_url);
 
         return match request {
             Ok(f) =>
                 match f.json::<SubscriberRequest>().await {
                     Ok(x) => Ok(x),
                     Err(y) => Err(compose_error_response(Status::NotAcceptable, y.to_string())),
+                }
+            Err(e) => Err(compose_error_response(Status::NotFound, e.to_string())),
+        };
+    }
+
+    pub fn unsubscribe(product_type: &str) -> Result<SubscriberRequest> {
+        let product_type_clone = String::from(product_type);
+        return thread
+            ::spawn(move || Self::unsubscribe_request(product_type_clone))
+            .join()
+            .unwrap();
+    }
+
+    #[tokio::main]
+    async fn unsubscribe_request(product_type: String) -> Result<SubscriberRequest> {
+        let product_type_upper: String = product_type.to_uppercase();
+        let product_type_str: &str = product_type_upper.as_str();
+        let notification_receiver_url: String = format!(
+            "{}/receive",
+            APP_CONFIG.get_instance_root_url()
+        );
+
+        let request_url: String = format!(
+            "{}/notification/unsubscribe/{}?url={}",
+            APP_CONFIG.get_publisher_root_url(),
+            product_type_str,
+            notification_receiver_url
+        );
+        let request = REQWEST_CLIENT.post(request_url.clone())
+            .header("Content-Type", "application/json")
+            .header("Accept", "application/json")
+            .send().await;
+        log::warn_!("Sent unsubscribe request to: {}", request_url);
+
+        return match request {
+            Ok(f) =>
+                match f.json::<SubscriberRequest>().await {
+                    Ok(x) => Ok(x),
+                    Err(_) =>
+                        Err(
+                            compose_error_response(
+                                Status::NotFound,
+                                String::from("Already unsubscribed to the topic.")
+                            )
+                        ),
                 }
             Err(e) => Err(compose_error_response(Status::NotFound, e.to_string())),
         };
